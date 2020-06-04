4ddef3f7c33977a39de7be6619ace5c9d20bbe01
sobhanbehuria-git <57714191+sobhanbehuria-git@users.noreply.github.com>
2020-06-04 22:23:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to Prod Env');
+  res.send('Jenkins PROD Deployment');
 });
 
 app.listen(PORT, HOST);

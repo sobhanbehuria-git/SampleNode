57fd09a8e2151ce4d4609e484841cfd106b93094
sobhanbehuria-git <57714191+sobhanbehuria-git@users.noreply.github.com>
2020-06-05 00:29:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Dev Deployment successfully done');
+  res.send('Dev Deployment successfully done.!!!!');
 });
 
 app.listen(PORT, HOST);
